a8b998f05bea767d601eb2161ee642d23bdb4e6d
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-09-05 19:04:26 +0200

    Updates

---
+++ precache-manifest.3a536f7573a7d31d0eb11e71138f188c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc3359b63bc3bb7f33071c0c71e2b42",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "70f19013f5cf060bd743",
+    "url": "/demo/static/css/main.757e0e5f.chunk.css"
+  },
+  {
+    "revision": "060196f3e7f348793ed9",
+    "url": "/demo/static/js/2.1796239c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo/static/js/2.1796239c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f19013f5cf060bd743",
+    "url": "/demo/static/js/main.3de5736d.chunk.js"
+  },
+  {
+    "revision": "b65806e6d9f59f46a847",
+    "url": "/demo/static/js/runtime-main.48a0feb4.js"
+  },
+  {
+    "revision": "87ff9c415a5df2626b573f1dd89c2863",
+    "url": "/demo/static/media/logo.87ff9c41.png"
+  }
+]);
